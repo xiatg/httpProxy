2c06f97cd4c41e7d079197a6fa0ac504a4bc8593
Tongguang Watanabe Xia <ghbq674521@163.com>
2021-10-17 09:19:38 +0800

    2nd commit

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
  * @Date: 2021-10-09 08:57:47
- * @LastEditTime: 2021-10-09 14:23:50
+ * @LastEditTime: 2021-10-12 11:10:44
  * @Description: Proxy
  * @FilePath: /CS3103Proxy/proxy.c
  */
@@ -12,19 +12,7 @@
 #include <unistd.h>
 #include <string.h>
 
-int main(int argc, char *argv[]){
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s <port> <flag_telemetry> <filename of blacklist>\n", argv[0]); // stdio.h
-        exit(EXIT_FAILURE); // stdlib.h
-    }
-
-    int port = atoi(argv[1]);
-    int flag_telemetry = atoi(argv[2]);
-    char *b_list_path = argv[3];
-
-    // Debug input
-    // printf("%d\n%d\n%s\n", port, flag_telemetry, b_list_path);
-
+int init_server_socket(int port) {
     struct sockaddr_in Saddress; // IPv4 Address netinet/in.h
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0); // Socket Creation sys/socket.h
@@ -57,28 +45,93 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in Caddress;
-    int new_socket;
+    return sockfd;
+}
 
-    socklen_t clen = sizeof(Caddress);
-    if ((new_socket = accept(sockfd, (struct sockaddr_in *) &Caddress, &clen)) < 0) {
-        perror("Accept failed.");
-        exit(EXIT_FAILURE);
+// int init_client_socket() {
+
+// }
+
+int read_request_line(int sock, char *buffer, int buf_size) {
+    int i = 0;
+    char c = '\0';
+    int n;
+
+    while ((i < buf_size - 1) && (c != '\n')) {
+        n = recv(sock, &c, 1, 0); // receive one char from the socket
+        buffer[i] = c;
+        i++;
+
+        //Debug: print received msg
+        printf("%c", c);
     }
 
-    int valread;
-    int buffer_size = 1024; // Buffer size;
-    char buffer[buffer_size];
-    
-    valread = read(new_socket, buffer, sizeof(buffer)); // unistd.h
-    
-    // Debug Listen Value
-    printf("%s\n", buffer);
-    
-    // Debug Send Back
-    char *hello = "Hello World from server";
-    send(new_socket, hello, strlen(hello), 0); // string.h
-    printf("Hello sent!\n");
+    // while (buffer[i-1] == '\n' || )
+
+    return i;
+}
+
+void *process_request(void * _client_sock) {
+    int client_sock = (int) (long) _client_sock;
+
+    // Parse the request
+    char line[10000]; // TODO: Length?
+    char method[10000];
+    char url[10000];
+    char protocol[10000];
+    char host[10000];
+    char path[10000];
+    char headers[20000];
+    int line_len = read_request_line(client_sock, line, sizeof(line));
+
+    if ((sscanf(line, "%[^ ] %[^ ] %[^ ]", method, url, protocol)))
+
+
+    // while (read_request_line(client_sock, line, sizeof(line)) > 0) {
+
+    // }
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <port> <flag_telemetry> <filename of blacklist>\n", argv[0]); // stdio.h
+        exit(EXIT_FAILURE); // stdlib.h
+    }
+
+    int port = atoi(argv[1]);
+    int flag_telemetry = atoi(argv[2]);
+    char *b_list_path = argv[3];
+
+    // Debug input
+    // printf("%d\n%d\n%s\n", port, flag_telemetry, b_list_path);
+
+    int server_sock = init_server_socket(port);
+    printf(";) HTTPS Proxy running on port %d!\n", port);
+
+    struct sockaddr_in Caddress;
+    socklen_t clen = sizeof(Caddress);
+    int client_sock;
+
+    while (1) {
+        if ((client_sock = accept(server_sock, (struct sockaddr_in *) &Caddress, &clen)) < 0) {
+            perror("Accept failed.");
+            exit(EXIT_FAILURE);
+        }
+
+        int valread;
+        int buffer_size = 1024; // Buffer size;
+        char buffer[buffer_size];
+        
+        // Debug Listen Value
+        // valread = read(client_sock, buffer, sizeof(buffer)); // unistd.h
+        // printf("%s\n", buffer);
+
+        process_request((void *) (long) client_sock); // Void Pointer <== Long (same-size) <== Int
+    }
+
+    close(server_sock);
     
     return 0;
 }
