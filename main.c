b76d3c4bfb230809495af94d356b3b0b9899c667
Tongguang Watanabe Xia <ghbq674521@163.com>
2021-10-18 23:35:50 +0800

    stable1.0

--- main.c
+++ main.c
@@ -1,10 +1,3 @@
-/*
- * @Date: 2021-10-09 08:57:47
- * @LastEditTime: 2021-10-12 11:10:44
- * @Description: Proxy
- * @FilePath: /CS3103Proxy/proxy.c
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -15,15 +8,24 @@
 #include <netdb.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include <ctype.h>
 
 #define HTTP10 "HTTP/1.0"
 #define RFC1123FMT "%a, %d, %b, %Y, %H:%M:%S GMT"
-#define TIMEOUT 300
+#define TIMEOUT 5 // in seconds
+#define THREADS_NUM 8
+
+// #define DEBUG
+
+int flag_telemetry = 0;
 
 int client_sock_stack[10000];
 int client_sock_top = -1;
 
-pthread_t threadpool[8];
+char *blacklist[100];
+int num_black_list = 0;
+
+pthread_t threadpool[THREADS_NUM];
 pthread_mutex_t client_sock_stack_lock;
 pthread_cond_t client_sock_stack_empty;
 
@@ -31,31 +33,56 @@ void send_headers(int client, int status, char *title, char *extra_header, char 
     char buffer[10000];
 
     // (HTTP/1.x) (200) (OK)
-    snprintf(buffer, sizeof(buffer), "%s %d %s\r\n", protocol, status, title);
+    snprintf(buffer, 10000, "%s %d %s\r\n", protocol, status, title);
     send(client, buffer, strlen(buffer), 0);
+
+#ifdef DEBUG
+    printf("[Send Header] HERE! 1\n");
+#endif
 
     // Date
     time_t now = time((time_t*) 0);
     char time_buffer[100];
-    strftime(time_buffer, sizeof(time_buffer), RFC1123FMT, gmtime(&now));
-    snprintf(buffer, sizeof(buffer), "Date: %s\r\n", time_buffer);
+    strftime(time_buffer, 100, RFC1123FMT, gmtime(&now));
+    snprintf(buffer, 10000, "Date: %s\r\n", time_buffer);
     send(client, buffer, strlen(buffer), 0);
+
+#ifdef DEBUG
+    printf("[Send Header] HERE! 2\n");
+#endif
 
     // Context-Type
     if (content_type != (char *) 0) {
-        snprintf(buffer, sizeof(buffer), "Content-Type: %s\r\n", content_type);
+        snprintf(buffer, 10000, "Content-Type: %s\r\n", content_type);
         send(client, buffer, strlen(buffer), 0);
     }
+
+#ifdef DEBUG
+    printf("[Send Header] HERE! 3\n");
+#endif
 
     // Content-Length
     if (length >= 0) {
-        snprintf(buffer, sizeof(buffer), "Content-Length: %d\r\n", length);
+        snprintf(buffer, 10000, "Content-Length: %d\r\n", length);
         send(client, buffer, strlen(buffer), 0);
     }
 
+#ifdef DEBUG
+    printf("[Send Header] HERE! 4\n");
+#endif
+
     // \r\n END
-    snprintf(buffer, sizeof(buffer), "\r\n");
-    send(client, buffer, strlen(buffer), 0);
+    snprintf(buffer, 10000, "\r\n");
+
+#ifdef DEBUG
+    printf("[Send Header] HERE! 5\n");
+#endif
+
+    send(client, buffer, 2, 0);
+
+#ifdef DEBUG
+    printf("[Send Header] HERE! 6\n");
+#endif
 }
 
 void raise_error(int client, int status, char *title, char *protocol){
@@ -106,9 +133,11 @@ int init_client_socket(int client_sock, char* host, int port, char *protocol) {
 
     if (sockfd < 0) {
         // Target Socket Creation Failed
-        raise_error(client_sock, 500, "Internal Error", protocol);
-
+#ifdef DEBUG
         perror("Target Socket creation failed.");
+#endif
+
+        raise_error(client_sock, 500, "Internal Error", protocol);
         return -1;
     }
 
@@ -116,9 +145,10 @@ int init_client_socket(int client_sock, char* host, int port, char *protocol) {
 
     if ((he = gethostbyname(host)) == NULL) {
         // Unknown host
-        raise_error(client_sock, 404, "Not Found", protocol);
-
+#ifdef DEBUG
         perror("Unknown target host.");
+#endif
+        raise_error(client_sock, 404, "Not Found", protocol);
         return -1;
     }
 
@@ -129,9 +159,10 @@ int init_client_socket(int client_sock, char* host, int port, char *protocol) {
 
     if (connect(sockfd, (struct sockaddr*) &TAddress, sizeof(TAddress)) < 0) {
         //Target connection failed
-        raise_error(client_sock, 503, "Service Unavailable", protocol);
-
+#ifdef DEBUG
         perror("Target connection failed.");
+#endif
+        raise_error(client_sock, 503, "Service Unavailable", protocol);
         return -1;
     }
 
@@ -150,7 +181,7 @@ int read_request_line(int sock, char *buffer, int buf_size) {
         i++;
 
         //Debug: print received msg
-        printf("%c", c);
+        // printf("%c", c);
     }
 
     while (buffer[i-1] == '\n' || buffer[i-1] == '\r') {
@@ -161,11 +192,11 @@ int read_request_line(int sock, char *buffer, int buf_size) {
     return i;
 }
 
-void proxy_https(int client, char* method, char* host, char* protocol, char* headers, FILE* sockrfp, FILE* sockwfp) {
+long proxy_https(int client, char* method, char* host, char* protocol, char* headers, FILE* sockrfp, FILE* sockwfp) {
     //HTTP/1.x 200 Connection established\r\n\r\n
     char connection_established[100];
 
-    snprintf(connection_established, sizeof(connection_established), "%s 200 Connection established\r\n\r\n", protocol);
+    snprintf(connection_established, 100, "%s 200 Connection established\r\n\r\n", protocol);
     
     send(client, connection_established, strlen(connection_established), 0);
 
@@ -188,7 +219,13 @@ void proxy_https(int client, char* method, char* host, char* protocol, char* hea
 
     fd_set fdset;
     int r;
-    char buffer[10000];
+    char buffer[1024];
+
+#ifdef DEBUG
+    printf("(Tunnel) Tunneling for client sock %d & target sock %d\n", client, server_read_fd);
+#endif
+
+    long byte_count = 0;
 
     while (1){
         FD_ZERO(&fdset);
@@ -198,8 +235,13 @@ void proxy_https(int client, char* method, char* host, char* protocol, char* hea
         r = select(maxfd, &fdset, (fd_set*) 0, (fd_set*) 0, &timeout);
 
         if (r == 0) {
-            raise_error(client, 408, "Request Timeout", protocol);
-            return;
+            //Time out
+
+#ifdef DEBUG
+            printf("(Tunnel) Timeout for client sock %d & target sock %d\n", client, server_read_fd);
+#endif
+
+            return byte_count;
         } else if (FD_ISSET(client_read_fd, &fdset)) {
             r = read(client_read_fd, buffer, sizeof(buffer));
             if (r <= 0) {
@@ -219,10 +261,18 @@ void proxy_https(int client, char* method, char* host, char* protocol, char* hea
                 break;
             }
         }
+
+        byte_count += r;
+
+        //Debug
+        // printf("(Tunnel) client %d <--> target %d: %d bytes\n", client, server_read_fd, r);
     }
 
-    //Debug
-    printf("https ends\n");
+#ifdef DEBUG
+    printf("(Tunnel) Normal End for client sock %d & target sock %d\n", client, server_read_fd);
+#endif
+
+    return byte_count;
 }
 
 void *process_request(void * _client_sock) {
@@ -240,12 +290,33 @@ void *process_request(void * _client_sock) {
 
     // Wrong format
     if (sscanf(line, "%[^ ] %[^ ] %[^ ]", method, url, protocol) != 3) {
+
+#ifdef DEBUG
+        perror("Unknown HTTP Request First Line Format:");
+        printf("%s\n", line);
+#endif
+
+        //TODO
+        if (strlen(line) == 0) {
+            /*
+            TODO:
+            a very weired bug: getting an empty http request from the 
+            browser and calling send() back to the browser will crash
+            the program.
+            */
+            return NULL;
+        }
+
         raise_error(client_sock, 400, "Bad Request", HTTP10);
         return NULL;
     }
 
     // Null URL
     if (url[0] == '\0') {
+
+#ifdef DEBUG
+        perror("NULL URL");
+#endif
         raise_error(client_sock, 400, "Bad Request", protocol);
         return NULL;
     }
@@ -256,13 +327,35 @@ void *process_request(void * _client_sock) {
     if (strcmp(method, "CONNECT") == 0) {
         // URL without :port
         if (sscanf(url, "%[^:]:%d", host, &port) != 2) {
+            
+#ifdef DEBUG
+            perror("URL without :port");
+#endif
+
             raise_error(client_sock, 400, "Bad Request", protocol);
             return NULL;
         } 
     } else {
-        // Unknown method (NOT CONNECT)
+
+#ifdef DEBUG
+        perror("method != CONNECT");
+#endif
+
         raise_error(client_sock, 400, "Bad Request", protocol);
         return NULL;
+    }
+
+    //Blacklisting
+    int i = 0;
+    for (i = 0; i < num_black_list; i++) {
+        if (strstr(host, blacklist[i]) != NULL) {
+#ifdef DEBUG
+            perror("blacklist triggered!");
+            printf("Blocked host: %s\n", host);
+#endif
+            raise_error(client_sock, 403, "Blacklisted URL", protocol);
+            return NULL;
+        }
     }
 
     //Parse the headers
@@ -270,6 +363,10 @@ void *process_request(void * _client_sock) {
     int headers_len = 0;
     while (read_request_line(client_sock, line, sizeof(line)) > 0) {
         int line_len = strlen(line);
+
+        //Debug
+        // printf("HEADER LENGTH LOL = %d\n", headers_len);
+
         memcpy(&headers[headers_len], line, line_len);
         headers_len += line_len;
     }
@@ -277,26 +374,35 @@ void *process_request(void * _client_sock) {
 
     int target_sock = init_client_socket(client_sock, host, port, protocol);
 
+#ifdef DEBUG
+    printf("(HTTPS Proxy) initialized client sock %d to target sock %d\n", client_sock, target_sock);
+#endif
+
     if (target_sock >= 0) {
         FILE* sockrfp;
         FILE* sockwfp;
         sockrfp = fdopen(target_sock, "r");
         sockwfp = fdopen(target_sock, "w");
         
-        proxy_https(client_sock, method, host, protocol, headers, sockrfp, sockwfp);
+        clock_t start, end; 
+        start = clock();
+        long byte_count = proxy_https(client_sock, method, host, protocol, headers, sockrfp, sockwfp);
+
+        if (flag_telemetry == 1 && byte_count >= 0) {
+            end = clock();
+            printf("Hostname: %s, Size: %ld bytes, Time: %.3f sec\n", host, byte_count, (float) (end-start) / 1000.0);
+        }
 
         close(target_sock);
     }
-
-    close(client_sock);
     return NULL;
 }
 
 void *thread_control(void *arg) {
     int index = (int) (long) arg;
+    int client_sock;
 
     while (1) {
-        int client_sock;
         pthread_mutex_lock(&client_sock_stack_lock);
         while (client_sock_top == -1) {
             pthread_cond_wait(&client_sock_stack_empty, &client_sock_stack_lock);
@@ -305,14 +411,61 @@ void *thread_control(void *arg) {
         client_sock_top -= 1;
         pthread_mutex_unlock(&client_sock_stack_lock);
 
-        //Debug
-        printf("Thread %d processing request!\nStack_top = %d\n", index, client_sock_top);
+#ifdef DEBUG
+        printf("(Thread %d) Processing! client sock = %d, Stack_top = %d\n", index, client_sock, client_sock_top);
+#endif
 
         process_request((void *) (long) client_sock); // Void Pointer <== Long (same-size) <== Int
+
+        close(client_sock);
     
-        //Debug
-        printf("Thread %d completed!\n", index);
+#ifdef DEBUG
+        printf("(Thread %d) Completed! client sock = %d, Stack_top = %d\n", index, client_sock, client_sock_top);
+#endif
     }
+}
+
+int is_number(char *s) {
+    int i = 0;
+    while(s[i] != '\0') {
+        if (isdigit(s[i]) == 0) {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+int read_black_list(char *file_path) {
+    int num_black = 0;
+
+    FILE * fp;
+    char *line = NULL;
+    size_t line_len = 0;
+    ssize_t nread;
+    fp = fopen(file_path, "r");
+    if (fp == NULL) {
+        printf("failed to load blacklist.txt\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((nread = getline(&line, &line_len, fp)) != -1) {
+
+        while (line[nread - 1] == '\n'){
+            line[nread - 1] = '\0';
+            nread--;
+        }
+
+        //Debug
+        // printf("%s\n", line);
+        // printf("%p\n", blacklist[num_black]);
+        blacklist[num_black] = malloc(nread);
+        strcpy(blacklist[num_black], line);
+        num_black++;
+    }
+    
+    fclose(fp);
+    return num_black;
 }
 
 int main(int argc, char *argv[]) {
@@ -322,8 +475,30 @@ int main(int argc, char *argv[]) {
     }
 
     int port = atoi(argv[1]);
-    int flag_telemetry = atoi(argv[2]);
+
+    //Checking the correctness of flag_telemetry
+    if (strlen(argv[2]) > 1) {
+        printf("Wrong flag for telemetry, should be 0 or 1.\n");
+        exit(EXIT_FAILURE);
+    }
+    if (is_number(argv[2]) == 0) {
+        printf("Wrong flag for telemetry, should be 0 or 1.\n");
+        exit(EXIT_FAILURE);
+    }
+    flag_telemetry = atoi(argv[2]);
+    if (flag_telemetry != 0 && flag_telemetry != 1) {
+        printf("Wrong flag for telemetry, should be 0 or 1.\n");
+        exit(EXIT_FAILURE);
+    }
+
     char *b_list_path = argv[3];
+
+    num_black_list = read_black_list(b_list_path);
+
+    //Debug
+    // for (int i = 0; i < num_black_list; i++) {
+    //     printf("%s\n", blacklist[i]);
+    // }
 
     int server_sock = init_server_socket(port);
     printf(";) HTTPS Proxy running on port %d!\n", port);
@@ -335,7 +510,9 @@ int main(int argc, char *argv[]) {
     pthread_mutex_init(&client_sock_stack_lock, NULL);
     pthread_cond_init(&client_sock_stack_empty, NULL);
 
-    for (int i = 0; i < 8; i++) {
+    int i = 0;
+
+    for (i = 0; i < THREADS_NUM; i++) {
         pthread_create(&threadpool[i], NULL, &thread_control,(void *) (long) i);
     }
 
@@ -354,7 +531,7 @@ int main(int argc, char *argv[]) {
         pthread_mutex_unlock(&client_sock_stack_lock);
     }
 
-    for (int i = 0; i < 8; i++) {
+    for (i = 0; i < THREADS_NUM; i++) {
         pthread_join(threadpool[i], NULL);
     }
 
@@ -362,6 +539,5 @@ int main(int argc, char *argv[]) {
     pthread_cond_destroy(&client_sock_stack_empty);
 
     close(server_sock);
-    
     return 0;
 }
